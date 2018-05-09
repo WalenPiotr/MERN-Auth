44c175b855371999e68b03639b3f0793806570e6
Piotr Walen <walen.piotr@gmail.com>
2018-05-09 13:21:28 +0200

    Add export

--- server/models/index.js
+++ server/models/index.js
@@ -4,4 +4,4 @@ mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/calories-backend', {
     keepAlive: true
 });
-module.exports.User = require('./user');
+module.exports.User = require('./user.js');
