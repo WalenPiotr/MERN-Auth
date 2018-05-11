fd6a8f1d8a0e5babbe5a1dee66671fbbc2a7ca9d
Piotr Walen <walen.piotr@gmail.com>
2018-05-12 00:12:47 +0200

    Style status bar

--- client/src/components/StatusBar.js
+++ client/src/components/StatusBar.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Bar = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 50px;
+    background-color: rgba(0, 0, 0, 0);
+    border-radius: 8px;
+`;
+
+const ErrorBar = styled(Bar)`
+    background-color: rgba(250, 0, 0, 0.2);
+`;
+
+const SuccessBar = styled(Bar)`
+    background-color: rgba(0, 250, 0, 0.2);
+`;
 
 const StatusBar = ({ status }) => {
-    let component = <div> </div>;
-    if (status.error) {
-        component = <div> {status.error.message} </div>;
-    } else if (status.success) {
-        component = <div> {status.success.message} </div>;
+    let component = <Bar> </Bar>;
+    console.log(status);
+    if (status.error && Object.keys(status.error).length !== 0) {
+        component = <ErrorBar> {status.error.message} </ErrorBar>;
+    } else if (status.success && Object.keys(status.success).length !== 0) {
+        component = <SuccessBar> {status.success.message} </SuccessBar>;
     }
     return component;
 };
