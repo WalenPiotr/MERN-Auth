77f00e77f5d8afe6e23adb4ee68d85b5198aff40
Piotr Walen <walen.piotr@gmail.com>
2018-05-12 11:31:11 +0200

    Add link for home and protected routes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,19 +4,30 @@ import styled from 'styled-components';
 
 const Bar = styled.div`
     display: flex;
-    justify-content: flex-end;
+    justify-content: space-between;
     width: 100%;
     height: 50px;
     background-color: rgba(10, 40, 220, 0.8);
     border-radius: 0 0 8px 8px;
 `;
 
+const Navigation = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin-left: 10px;
+`;
+const Authentication = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin-right: 10px;
+`;
+
 const LinkBox = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-right: 15px;
     height: 100%;
+    margin: 0 10px;
 `;
 
 const StyledLink = styled(Link)`
@@ -26,16 +37,16 @@ const StyledLink = styled(Link)`
 `;
 
 const Navbar = ({ authenticated, clearStatus }) => {
-    const component = authenticated ? (
-        <Bar>
+    const authComponents = authenticated ? (
+        <Authentication>
             <LinkBox>
                 <StyledLink to="/logout" onClick={() => clearStatus()}>
                     Logout
                 </StyledLink>
             </LinkBox>
-        </Bar>
+        </Authentication>
     ) : (
-        <Bar>
+        <Authentication>
             <LinkBox>
                 <StyledLink to="/login" onClick={() => clearStatus()}>
                     Login
@@ -46,9 +57,30 @@ const Navbar = ({ authenticated, clearStatus }) => {
                     Register
                 </StyledLink>
             </LinkBox>
+        </Authentication>
+    );
+
+    const navigationComponents = (
+        <Navigation>
+            <LinkBox>
+                <StyledLink to="/" onClick={() => clearStatus()}>
+                    Home
+                </StyledLink>
+            </LinkBox>
+            <LinkBox>
+                <StyledLink to="/protected" onClick={() => clearStatus()}>
+                    Protected
+                </StyledLink>
+            </LinkBox>
+        </Navigation>
+    );
+
+    return (
+        <Bar>
+            {navigationComponents}
+            {authComponents}
         </Bar>
     );
-    return component;
 };
 
 export default Navbar;
