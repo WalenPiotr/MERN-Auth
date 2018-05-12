bafc689b594bceb67dfab102801568861a67a7c9
Piotr Walen <walen.piotr@gmail.com>
2018-05-12 10:27:53 +0200

    Style register and login forms

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-
-class LoginForm extends Component {
+import styled from 'styled-components';
+export default class LoginForm extends Component {
     state = {
         username: '',
         password: ''
@@ -28,24 +28,67 @@ class LoginForm extends Component {
             return newState;
         });
     };
+
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label> Login Form </label>
-                <input
+            <Form onSubmit={this.handleSubmit}>
+                <Label> Login </Label>
+                <Input
                     type="text"
                     value={this.state.username}
                     onChange={this.handleChange('username')}
+                    placeholder="username"
                 />
-                <input
+                <Input
                     value={this.state.password}
                     type="password"
                     onChange={this.handleChange('password')}
+                    placeholder="password"
                 />
-                <button type="submit">Submit</button>
-            </form>
+                <Button type="submit">Submit</Button>
+            </Form>
         );
     }
 }
+const Label = styled.label`
+    margin-top: 10px;
+    margin: 10px auto 0px;
+`;
 
-export default LoginForm;
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    border: 1px solid grey;
+    border-radius: 4px;
+    margin: 15px auto 0px;
+`;
+
+const Input = styled.input`
+    border: none;
+    background-image: none;
+    background-color: transparent;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+    height: 30px;
+    margin: 10px 10px 0px;
+    border-radius: 4px;
+    text-indent: 8px;
+    border: 1px solid grey;
+`;
+
+const Button = styled.button`
+    border: none;
+    background-image: none;
+    background-color: transparent;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+
+    height: 30px;
+    margin: 10px 10px 10px;
+    border-radius: 4px;
+    background-color: rgba(10, 40, 220, 0.8);
+    color: white;
+`;
--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { clearStatus } from '../actions/status';
-
-class RegisterForm extends Component {
+import styled from 'styled-components';
+export default class RegisterForm extends Component {
     state = {
         username: '',
         password: ''
@@ -29,22 +29,65 @@ class RegisterForm extends Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label> Register Form </label>
-                <input
+            <Form onSubmit={this.handleSubmit}>
+                <Label> Register Form </Label>
+                <Input
                     type="text"
                     value={this.state.username}
                     onChange={this.handleChange('username')}
+                    placeholder="username"
                 />
-                <input
+                <Input
                     value={this.state.password}
                     type="password"
                     onChange={this.handleChange('password')}
+                    placeholder="password"
                 />
-                <button type="submit">Submit</button>
-            </form>
+                <Button type="submit">Submit</Button>
+            </Form>
         );
     }
 }
 
-export default RegisterForm;
+const Label = styled.label`
+    margin-top: 10px;
+    margin: 10px auto 0px;
+`;
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    border: 1px solid grey;
+    border-radius: 4px;
+    margin: 15px auto 0px;
+`;
+
+const Input = styled.input`
+    border: none;
+    background-image: none;
+    background-color: transparent;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+    height: 30px;
+    margin: 10px 10px 0px;
+    border-radius: 4px;
+    text-indent: 8px;
+    border: 1px solid grey;
+`;
+
+const Button = styled.button`
+    border: none;
+    background-image: none;
+    background-color: transparent;
+    -webkit-box-shadow: none;
+    -moz-box-shadow: none;
+    box-shadow: none;
+
+    height: 30px;
+    margin: 10px 10px 10px;
+    border-radius: 4px;
+    background-color: rgba(10, 40, 220, 0.8);
+    color: white;
+`;
